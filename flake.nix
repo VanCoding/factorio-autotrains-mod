330dcd6a5f623b34c0148bf331f88dfae14cfb8f
Patrik Stutz <patrik.stutz@gmail.com>
2022-06-27 21:30:24 +0200

    made it a nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Factorio Autotrains Mod";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
